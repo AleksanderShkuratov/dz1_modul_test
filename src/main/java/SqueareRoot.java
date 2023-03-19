09a27730fa2deeca2ade89e91e67fb449d548d18
Aleksander Shkuratov <shk888@yandex.ru>
2023-03-19 22:49:16 +0300

    Посмотреть какие еще значения могут принимать числа типа double, кроме числовых и написать тест с их использованием на все коэффициенты. solve должен выбрасывать исключение.

--- src/main/java/SqueareRoot.java
+++ src/main/java/SqueareRoot.java
@@ -2,6 +2,11 @@ public class SqueareRoot {
 
     double[] solve(double a, double b, double c, double epsilon) throws Exception {
 
+
+        if(Double.isNaN(Math.abs(a)) || Math.abs(a) == Double.NEGATIVE_INFINITY || Math.abs(a) == Double.POSITIVE_INFINITY){
+            throw  new Exception();
+        }
+
         if(!(Math.abs(a) >= epsilon)){
            throw  new Exception();
         }
@@ -11,26 +16,27 @@ public class SqueareRoot {
         // discriminant < 0
         if (D < -epsilon) {
             return new double[0];
-        } else if (D > epsilon) {
+
+            // discriminant > 0 но меньше эпсилона
+        } else if (Math.abs(D) < epsilon) {
             double[] result = new double[2];
-
-            // не работает формула из доп. материалов стр.20
-            // при b = 0.0 Math.signum(b) получается равным 0.0
-            // дальнейшее умножение на Math.sqrt(D) и деление на 2 приводит к результату x1 = -0.0 и x2 = Infinity
-//            result[0] = -(b + Math.signum(b) * Math.sqrt(D)) / 2;
-//            result[1] = c / x1;
-
-            // "школьная" формула работает
-            result[0] = (-b + Math.sqrt(D)) / 2;
-            result[1] = (-b - Math.sqrt(D)) / 2;
-
+            result[0] = -b / (2 * a);
+            result[1] = -b / (2 * a);
             return result;
         }
 
+        // discriminant > 0 и больше эпсилона
         double[] result = new double[2];
-        result[0] = -b / (2 * a);
-        result[1] = -b / (2 * a);
 
+        // не работает формула из доп. материалов стр.20
+        // при b = 0.0 Math.signum(b) получается равным 0.0
+        // дальнейшее умножение на Math.sqrt(D) и деление на 2 приводит к результату x1 = -0.0 и x2 = Infinity
+//            result[0] = -(b + Math.signum(b) * Math.sqrt(D)) / 2;
+//            result[1] = c / x1;
+
+        // "школьная" формула работает
+        result[0] = (-b + Math.sqrt(D)) / 2;
+        result[1] = (-b - Math.sqrt(D)) / 2;
         return result;
     }
 
--- src/test/java/SqueareRootTest.java
+++ src/test/java/SqueareRootTest.java
@@ -16,7 +16,6 @@ class SqueareRootTest {
         double a = 1;
         double b = 0;
         double c = 1;
-
         double epsilon = 1.0e-10;
 
         // act
@@ -43,7 +42,6 @@ class SqueareRootTest {
         double a = 1;
         double b = 0;
         double c = -1;
-
         double epsilon = 1.0e-10;
 
         // act
@@ -72,7 +70,6 @@ class SqueareRootTest {
         double a = 1;
         double b = 2;
         double c = 1;
-
         double epsilon = 1.0e-10;
 
         // act
@@ -100,12 +97,8 @@ class SqueareRootTest {
         Assertions.assertThrows(Exception.class, ()->{
             // arrange
             double a = 0;
-            Random r = new Random();
-            double rangeMin = 0.0d;
-            double rangeMax = 10.0d;
-            double b = rangeMin + (rangeMax - rangeMin) * r.nextDouble();
-            double c = rangeMin + (rangeMax - rangeMin) * r.nextDouble();
-
+            double b = getDoubleRandom();
+            double c = getDoubleRandom();
             double epsilon = 1.0e-10;
 
             // act
@@ -114,6 +107,163 @@ class SqueareRootTest {
 
         });
 
+    }
+
+    @Test
+    void solveTest5() throws Exception{
+
+        // подобрать такие коэффициенты квадратного уравнения для случая одного корня кратности два,
+        // чтобы дискриминант был отличный от нуля, но меньше заданного эпсилон.
+
+        // arrange
+        // выбираем данные из заданной функции
+
+        double a = 1;
+        double b = 2.01;
+        double c = 1;
+        double epsilon = 1.0e-1;
+
+        // act
+        SqueareRoot squeare = new SqueareRoot();
+        double[] result = squeare.solve(a, b, c, epsilon);
+
+        int length = result.length;
+
+        // assert
+        Assertions.assertEquals(2, length);
+        Assertions.assertEquals(-1.005, result[0]);
+        Assertions.assertEquals(-1.005, result[0]);
+
+    }
+
+    @Test
+    void solveTest6() throws Exception{
+
+        // Посмотреть какие еще значения могут принимать числа типа double,
+        // кроме числовых и написать тест с их использованием на все коэффициенты.
+        // solve должен выбрасывать исключение.
+
+        // assert a
+
+        Assertions.assertThrows(Exception.class, ()->{
+            // arrange
+            double a = Double.NaN;
+            double b = getDoubleRandom();
+            double c = getDoubleRandom();
+            double epsilon = 1.0e-10;
+
+            // act
+            SqueareRoot squeare = new SqueareRoot();
+            double[] result = squeare.solve(a, b, c, epsilon);
+        });
+
+        Assertions.assertThrows(Exception.class, ()->{
+            // arrange
+            double a = Double.NEGATIVE_INFINITY;
+            double b = getDoubleRandom();
+            double c = getDoubleRandom();
+            double epsilon = 1.0e-10;
+
+            // act
+            SqueareRoot squeare = new SqueareRoot();
+            double[] result = squeare.solve(a, b, c, epsilon);
+        });
+
+        Assertions.assertThrows(Exception.class, ()->{
+            // arrange
+            double a = Double.POSITIVE_INFINITY;
+            double b = getDoubleRandom();
+            double c = getDoubleRandom();
+            double epsilon = 1.0e-10;
+
+            // act
+            SqueareRoot squeare = new SqueareRoot();
+            double[] result = squeare.solve(a, b, c, epsilon);
+        });
+
+        // assert b
+
+        Assertions.assertThrows(Exception.class, ()->{
+            // arrange
+            double a = getDoubleRandom();
+            double b = Double.NaN;
+            double c = getDoubleRandom();
+            double epsilon = 1.0e-10;
+
+            // act
+            SqueareRoot squeare = new SqueareRoot();
+            double[] result = squeare.solve(a, b, c, epsilon);
+        });
+
+        Assertions.assertThrows(Exception.class, ()->{
+            // arrange
+            double a = getDoubleRandom();
+            double b = Double.NEGATIVE_INFINITY;
+            double c = getDoubleRandom();
+            double epsilon = 1.0e-10;
+
+            // act
+            SqueareRoot squeare = new SqueareRoot();
+            double[] result = squeare.solve(a, b, c, epsilon);
+        });
+
+        Assertions.assertThrows(Exception.class, ()->{
+            // arrange
+            double a = getDoubleRandom();
+            double b = Double.POSITIVE_INFINITY;
+            double c = getDoubleRandom();
+            double epsilon = 1.0e-10;
+
+            // act
+            SqueareRoot squeare = new SqueareRoot();
+            double[] result = squeare.solve(a, b, c, epsilon);
+        });
+
+        // assert c
+
+        Assertions.assertThrows(Exception.class, ()->{
+            // arrange
+            double a = getDoubleRandom();
+            double b = getDoubleRandom();
+            double c = Double.NaN;
+            double epsilon = 1.0e-10;
+
+            // act
+            SqueareRoot squeare = new SqueareRoot();
+            double[] result = squeare.solve(a, b, c, epsilon);
+        });
+
+        Assertions.assertThrows(Exception.class, ()->{
+            // arrange
+            double a = getDoubleRandom();
+            double b = getDoubleRandom();
+            double c = Double.NEGATIVE_INFINITY;
+            double epsilon = 1.0e-10;
+
+            // act
+            SqueareRoot squeare = new SqueareRoot();
+            double[] result = squeare.solve(a, b, c, epsilon);
+        });
+
+        Assertions.assertThrows(Exception.class, ()->{
+            // arrange
+            double a = getDoubleRandom();
+            double b = getDoubleRandom();
+            double c = Double.POSITIVE_INFINITY;
+            double epsilon = 1.0e-10;
+
+            // act
+            SqueareRoot squeare = new SqueareRoot();
+            double[] result = squeare.solve(a, b, c, epsilon);
+        });
+
+    }
+
+    private double getDoubleRandom(){
+        Random r = new Random();
+        double rangeMin = Double.MIN_VALUE;
+        double rangeMax = Double.MAX_VALUE;
+        return rangeMin + (rangeMax - rangeMin) * r.nextDouble();
     }
 
 
