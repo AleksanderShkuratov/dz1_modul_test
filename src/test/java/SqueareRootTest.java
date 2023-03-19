cd79b761550b2a0d8d51731f0c7d23c3f1e0fe81
Aleksander Shkuratov <shk888@yandex.ru>
2023-03-19 20:02:03 +0300

    тест2, который проверяет, что для уравнения x^2-1 = 0 есть два корня кратности 1 (x1=1, x2=-1)

--- src/test/java/SqueareRootTest.java
+++ src/test/java/SqueareRootTest.java
@@ -28,4 +28,35 @@ class SqueareRootTest {
 
     }
 
+    @Test
+    void solveTest2(){
+
+        // function x^2 - 1 = 0; сть два корня кратности 1 (x1=1, x2=-1)
+        // необходимо проверить что при x^2 - 1 = 0
+        // функция возвращает массив с 2-мя значениями 1 и -1
+
+        // arrange
+        // выбираем данные из заданной функции
+
+        double a = 1;
+        double b = 0;
+        double c = -1;
+
+        double epsilon = 1.0e-10;
+
+        // act
+        SqueareRoot squeare = new SqueareRoot();
+        double[] result = squeare.solve(a, b, c, epsilon);
+
+        int length = result.length;
+
+        // assert
+        Assertions.assertEquals(2, length);
+        Assertions.assertEquals(1, result[0]);
+        Assertions.assertEquals(-1, result[1]);
+
+    }
+
+
+
 }
