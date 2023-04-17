0f13a1b2ac2afed14fea04eae4414cbafc5d40d4
Aleksander Shkuratov <shk888@yandex.ru>
2023-04-17 17:00:20 +0300

    add Move tests

--- src/test/java/action/move/MoveTest.java
+++ src/test/java/action/move/MoveTest.java
@@ -1,15 +1,16 @@
 package action.move;
 
 import entity.Vector;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
@@ -41,6 +42,51 @@ class MoveTest {
         move.execute();
 
         verify(movable).setPosition(eq(new Vector(5, 8)));
+    }
+
+    @Test
+    @DisplayName("Попытка сдвинуть объект, у которого невозможно прочитать положение в пространстве, приводит к ошибке")
+    void test2() {
+        doThrow(new NullPointerException())
+                .when(movable)
+                .getPosition();
+
+        Assertions.assertThrows(Exception.class, () -> {
+            move.execute();
+        });
+    }
+
+    @Test
+    @DisplayName("Попытка сдвинуть объект, у которого невозможно прочитать значение мгновенной скорости, приводит к ошибке")
+    void test3() {
+        doReturn(new Vector(12, 5))
+                .when(movable)
+                .getPosition();
+        doThrow(new NullPointerException())
+                .when(movable)
+                .getVelocity();
+
+        Assertions.assertThrows(Exception.class, () -> {
+            move.execute();
+        });
+    }
+
+    @Test
+    @DisplayName("Попытка сдвинуть объект, у которого невозможно изменить положение в пространстве, приводит к ошибке")
+    void test4() {
+        doReturn(new Vector(12, 5))
+                .when(movable)
+                .getPosition();
+        doReturn(new Vector(-7, 3))
+                .when(movable)
+                .getVelocity();
+        doThrow(new RuntimeException())
+                .when(movable)
+                .setPosition(any());
+
+        Assertions.assertThrows(Exception.class, () -> {
+            move.execute();
+        });
     }
 
 }
